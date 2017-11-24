6c88b48b815f606b9c88b5b8462458c1f3e2f2e2
catsigma <im@catsigma.com>
2017-11-24 11:34:14 +0800

    using default rpc node

--- js/plugin.js
+++ js/plugin.js
@@ -134,7 +134,7 @@
   }
 
   const main = () => {
-    eztz.node.setProvider('https://teznode.catsigma.com')
+    // eztz.node.setProvider('https://teznode.catsigma.com')
     window.addEventListener('message', dispatcher)
   }
   main()
