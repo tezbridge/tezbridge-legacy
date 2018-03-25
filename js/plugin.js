c9f8ef9da36700d436d4d8801749b0810ef8c259
catsigma <im@catsigma.com>
2018-03-25 11:54:44 +0800

    fix plugin

--- js/plugin.js
+++ js/plugin.js
@@ -102,7 +102,17 @@ ${e.data.operations.map(x => x.kind + ' with ' + (x.amount || x.balance) + 'tz')
       },
       handler(e) {
         return rpc(() => {
-          const ops = e.data.operations.filter(x => x.kind === 'transaction' || x.kind === 'originate')
+          const ops = e.data.operations
+          .filter(x => x.kind === 'transaction' || x.kind === 'originate')
+          .map(x => {
+            if (x.amount)
+              x.amount = TZClient.r2tz(x.amount)
+
+            if (x.balance)
+              x.balance = TZClient.r2tz(x.balance)
+            
+            return x
+          })
           return tzclient.makeOperations([{
             kind: 'reveal',
             public_key: tzclient.key_pair.public_key
