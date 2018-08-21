e60b9c83ae021399ef00ae024b47641f6e67c9ea
catsigma <im@catsigma.com>
2018-08-21 16:28:56 +0800

    add remote signer

--- js/components.js
+++ js/components.js
@@ -6,6 +6,25 @@ if (!Promise.prototype.finally) {
   }
 }
 
+const rtc_info = {
+  local: {
+    candidates: [],
+    offer: {}
+  },
+  remote: {
+    candidates: [],
+    offer: {}
+  }
+}
+if (location.search) {
+  try {
+    const val = location.search.slice(1)
+    const remote_info = JSON.parse(new TextDecoder().decode(util.pako.inflate(util.base.decode(val))))
+    rtc_info.remote = remote_info
+  } catch(e) {
+  }
+}
+
 const getLocal = util.getLocal
 const setLocal = util.setLocal
 const removeLocal = util.removeLocal
@@ -76,21 +95,35 @@ components.Account = Vue.component('account', {
             <q-item-side>
               <q-btn flat @click="refreshBalance" icon="refresh" />
             </q-item-side>
-            </q-item>
-            <q-item>
-              <q-item-side icon="vpn key" />
-              <q-item-main>
-                <q-item-tile label>Access code</q-item-tile>
-                <q-item-tile sublabel>
-                  <p class="ellipsis" ref="access_code">
-                    {{access_code}}
-                  </p>
-                </q-item-tile>
-              </q-item-main>
-              <q-item-side>
-                <q-btn flat @click="genAccessCode" icon="unarchive" />
-              </q-item-side>
-            </q-item>
+          </q-item>
+          <q-item>
+            <q-item-side icon="vpn key" />
+            <q-item-main>
+              <q-item-tile label>Access code</q-item-tile>
+              <q-item-tile sublabel>
+                <p class="ellipsis" ref="access_code">
+                  {{access_code}}
+                </p>
+              </q-item-tile>
+            </q-item-main>
+            <q-item-side>
+              <q-btn flat @click="genAccessCode" icon="unarchive" />
+            </q-item-side>
+          </q-item>
+          <q-item>
+            <q-item-side icon="wifi tethering" />
+            <q-item-main>
+              <q-item-tile label>Remote signer</q-item-tile>
+              <q-item-tile sublabel>
+                <p class="ellipsis">
+                  
+                </p>
+              </q-item-tile>
+            </q-item-main>
+            <q-item-side>
+              <q-btn flat @click="openSigner" icon="create" />
+            </q-item-side>
+          </q-item>
         </q-list>
         <div class="center-wrapper">
           <q-btn color="cyan-8" outline @click="lock" label="Lock" icon="lock" />
@@ -107,6 +140,7 @@ components.Account = Vue.component('account', {
   props: ['account'],
   data() {
     return {
+      g: util.G,
       temp_secrets,
 
       locked: true,
@@ -134,6 +168,9 @@ components.Account = Vue.component('account', {
         })
       })
       .finally(() => this.loading = false)
+    },
+    openSigner() {
+      this.g.tzclient = this.tzclient
     },
     genAccessCode() {
       const random_iv = window.crypto.getRandomValues(new Uint8Array(12))
@@ -321,6 +358,7 @@ const genTZclient = (tzclient_param, account_name, password) => {
     return Promise.reject(err.toString())
   }
 }
+
 
 
 components.GenNewAccount = Vue.component('gen-new-account', {
@@ -791,5 +829,85 @@ components.Intro = Vue.component('intro', {
   }
 })
 
+components.RemoteSigner = Vue.component('remote-signer', {
+  template: `
+    <q-modal v-model="opened" content-css="padding: 24px; position: relative">
+      <div class="rtc-local-info" ref="rtc_local_info">{{local_info}}</div>
+      <div class="row justify-center">
+        <q-btn color="cyan-8" @click="copy_rtc_info" label="Copy" icon="content copy" outline />
+      </div>
+    </q-modal>
+  `,
+  props: ['tzclient'],
+  data() {
+    return {
+      opened: false,
+      local_info: ''
+    }
+  },
+  methods: {
+    copy_rtc_info() {
+      this.copyToClipboard(this.$refs.rtc_local_info, 'Connection info')
+    },
+    copyToClipboard(elem, name) {
+      const range = document.createRange()
+      const selection = window.getSelection()
+      range.selectNodeContents(elem)
+      selection.removeAllRanges()
+      selection.addRange(range)
+      document.execCommand("copy")
+      this.$q.notify({
+        color: 'positive',
+        icon: 'done',
+        timeout: 1500,
+        message: name + ' copied'
+      })
+    },
+  },
+  watch: {
+    tzclient() {
+      if (!this.tzclient) {
+        this.opened = false
+        return false
+      }
+
+      const conn = new RTCPeerConnection()
+      
+      if (!!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform)) {
+        alert('For remote signer connection\nmicrophone premission needs to be allowed\nit will only be active for 0.5 second.')
+        navigator.mediaDevices.getUserMedia({audio: true}).then(x => x.getAudioTracks()[0].stop())
+      }
+
+      conn.ondatachannel = (e) => {
+        e.channel.onmessage = () => {}
+      }
+      
+      conn.onicecandidate = e => {
+        rtc_info.local.candidates.push(e.candidate)
+      }
+
+      conn.setRemoteDescription(new RTCSessionDescription(rtc_info.remote.offer))
+      .then(() => conn.createAnswer())
+      .then(answer => {
+        rtc_info.local.offer = answer
+        conn.setLocalDescription(answer)
+      })
+      .then(() => {
+        rtc_info.remote.candidates.forEach(x => {
+          if (x)
+            conn.addIceCandidate(new RTCIceCandidate(x))
+        })
+      })
+      .then(() => {
+        this.opened = true
+        setTimeout(() => {
+          this.local_info = util.base.encode(util.pako.deflate(JSON.stringify(rtc_info.local)))
+        }, 1000)
+      })
+    }
+  },
+  mounted() {
+  }
+})
 
 module.exports = {components, intro_version}
--- js/util.js
+++ js/util.js
@@ -31,10 +31,17 @@ const devtoolsDetectListen = (() => {
 
 const host = 'https://mainnet.tezbridge.com'
 
+function base(u){for(var h={},i=u.length,g=u.charAt(0),r=0;r<u.length;r++){var e=u.charAt(r);if(void 0!==h[e])throw new TypeError(e+" is ambiguous");h[e]=r}function n(r){if("string"!=typeof r)throw new TypeError("Expected String");if(0===r.length)return Buffer.allocUnsafe(0);for(var e=[0],n=0;n<r.length;n++){var t=h[r[n]];if(void 0===t)return;for(var o=0,f=t;o<e.length;++o)f+=e[o]*i,e[o]=255&f,f>>=8;for(;0<f;)e.push(255&f),f>>=8}for(var a=0;r[a]===g&&a<r.length-1;++a)e.push(0);return Buffer.from(e.reverse())}return{encode:function(r){if(0===r.length)return"";for(var e=[0],n=0;n<r.length;++n){for(var t=0,o=r[n];t<e.length;++t)o+=e[t]<<8,e[t]=o%i,o=o/i|0;for(;0<o;)e.push(o%i),o=o/i|0}for(var f="",a=0;0===r[a]&&a<r.length-1;++a)f+=g;for(var h=e.length-1;0<=h;--h)f+=u[e[h]];return f},decodeUnsafe:n,decode:function(r){var e=n(r);if(e)return e;throw new Error("Non-base"+i+" character")}}}
+
 module.exports = {
+  G: {
+    tzclient: null
+  },
   devtoolsDetectListen,
   getLocal,
   setLocal,
   removeLocal,
-  host
+  host,
+  pako: window.pako,
+  base: base('1234567890qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP$-_.+!*(),')
 }
